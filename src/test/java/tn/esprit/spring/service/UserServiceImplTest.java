091b46d1114d141792ddf26c27c3505a64870834
Dhaker12 <dhaker.karous@esprit.tn>
2021-11-01 23:13:56 +0100

    test final

--- src/test/java/tn/esprit/spring/service/UserServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/UserServiceImplTest.java
@@ -59,8 +59,8 @@ public class UserServiceImplTest {
 		
 		@Test
 		public void testDeleteUser() {
-			us.deleteUser("44");
-			Assert.assertNull(us.retrieveUser("44"));
+			us.deleteUser("45");
+			Assert.assertNull(us.retrieveUser("45"));
 		// 5 tests unitaires  
 
 		}
