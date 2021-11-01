7bf2bcde4bfb13f06bec6fdb97dc67bfb394c860
zeineb <zeineb.bellahirich1@esprit.tn>
2021-11-01 22:31:57 +0100

    test jacoco

--- src/test/java/tn/esprit/spring/service/UserServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/UserServiceImplTest.java
@@ -59,8 +59,8 @@ public class UserServiceImplTest {
 		
 		@Test
 		public void testDeleteUser() {
-			us.deleteUser("130");
-			Assert.assertNull(us.retrieveUser("130"));
+			us.deleteUser("131");
+			Assert.assertNull(us.retrieveUser("131"));
 		}
 		
 		// 5 tests unitaires  
